129248142409cfc3492e6f105f9da5cf2b5e9603
Sean <smriedl@gmail.com>
2020-11-03 17:57:13 -0500

    Add user model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, :password, presence: true
+  validates :username, length: { in: 3..14 }, uniqueness: true
+  validates :password, length: { minimum: 3 }
 end
